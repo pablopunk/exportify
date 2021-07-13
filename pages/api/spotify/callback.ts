b4f27ebbaea4ac6795d0b58090e06d6b25050b58
Pablo P Varela <pablo@pablopunk.com>
2021-07-13 03:48:01 +0200

    I'm going crazy

--- pages/api/spotify/callback.ts
+++ pages/api/spotify/callback.ts
@@ -1,32 +1,71 @@
 import api from 'spotify/api'
-
-const MAX_LIMIT = 50
+import * as usetube from 'usetube'
 
 export default async function (req, res) {
   const code = req.query.code as string
 
-  if (!code) {
-    return res.status(400).send('Missing code')
-  }
-
   try {
+    if (!code) {
+      return res.status(400).send('Missing code')
+    }
+
     const data = await api.authorizationCodeGrant(code)
     const accessToken = data.body['access_token']
 
     api.setAccessToken(accessToken)
 
-    const results = await api.getMySavedTracks({ limit: MAX_LIMIT })
+    const apiLimit = 20
+    const results = await api.getMySavedTracks({ limit: apiLimit })
     const total = results.body['total']
     let songs = results.body.items.map((item) => item.track)
 
+    let songPromises = []
     if (total > songs.length) {
-      for (let offset = MAX_LIMIT; offset < total; offset += MAX_LIMIT) {
-        const results = await api.getMySavedTracks({ limit: MAX_LIMIT, offset })
-        songs = [...songs, ...results.body.items.map((item) => item.track)]
+      for (
+        let offset = apiLimit;
+        offset + apiLimit <= total;
+        offset += apiLimit
+      ) {
+        songPromises.push(api.getMySavedTracks({ limit: apiLimit, offset }))
       }
     }
 
-    return res.send(songs.map((song) => song.name))
+    songs = [
+      ...songs,
+      ...(await Promise.all(songPromises)).flatMap((item) =>
+        item.body.items.map((item) => item.track)
+      ),
+    ]
+
+    usetube.searchVideo(songs[0].name + ' ' + songs[0].artists[0].name)
+
+    const youtubeVideosPromises = songs.map((song) =>
+      usetube
+        .searchVideo(song.name + ' ' + song.artists[0].name)
+        .then((results) => ({
+          songId: song.id,
+          id: results?.videos?.[0]?.id,
+        }))
+    )
+
+    const youtubeVideos = await Promise.all(youtubeVideosPromises)
+
+    songs = songs.map((song) => {
+      const youtubeVideo: any = youtubeVideos.find(
+        (v: any) => v.songId === song.id
+      )
+
+      if (!youtubeVideo.id) {
+        return song
+      }
+
+      return {
+        ...song,
+        youtubeVideoId: youtubeVideo.id,
+      }
+    })
+
+    return res.status(200).send(songs)
   } catch (err) {
     return res.status(500).send(err.message)
   }
--- pages/songs.tsx
+++ pages/songs.tsx
@@ -1,48 +1,61 @@
-import { FunctionComponent } from 'react'
-import api from 'spotify/api'
+import { FunctionComponent, useEffect, useState } from 'react'
+import { useRouter } from 'next/router'
+import { FoldingCube } from 'better-react-spinkit'
 
-type Props = { songs: Array<any> }
+type Props = {}
 
-const Songs: FunctionComponent<Props> = ({ songs }) => (
-  <div>
-    <h1>Songs</h1>
-    <ul>
-      {songs.map((song) => (
-        <li key={song.id} className="flex items-center">
-          <div className="mr-2 text-sm">
-            {song.artists.map((a) => a.name).join(', ')}
-          </div>
-          <div className="text-lg font-semibold text-accent">{song.name}</div>
-        </li>
-      ))}
-    </ul>
-  </div>
-)
+const Songs: FunctionComponent<Props> = () => {
+  const { query } = useRouter()
+  const [code, setCode] = useState(null)
+  const [songs, setSongs] = useState([])
+
+  useEffect(() => {
+    if (code) {
+      fetch(`/api/spotify/callback?code=${code}`)
+        .then((response) => response.json())
+        .then((results) => {
+          setSongs(results)
+        })
+        .catch(() => {
+          // window.location.href = '/' // try to login again to refresh token
+        })
+    }
+  }, [code])
+
+  useEffect(() => {
+    if (query.code) {
+      setCode(query.code)
+    }
+  }, [query])
+
+  if (!code) {
+    return <div>Not found</div>
+  }
+
+  return (
+    <div>
+      {songs.length === 0 && (
+        <div className="flex flex-col items-center justify-center w-full">
+          <div className="my-5">Hang on! This might take a while...</div>
+          <FoldingCube
+            size={100}
+            color="var(--color-accent)"
+            className="mt-5"
+          />
+        </div>
+      )}
+      <ul>
+        {songs.map((song) => (
+          <li key={song.id} className="flex items-center">
+            <div className="mr-2 text-sm">
+              {song.artists.map((a) => a.name).join(', ')}
+            </div>
+            <div className="text-lg font-semibold text-accent">{song.name}</div>
+          </li>
+        ))}
+      </ul>
+    </div>
+  )
+}
 
 export default Songs
-
-export async function getServerSideProps(ctx) {
-  try {
-    const code = ctx.query.code
-    const data = await api.authorizationCodeGrant(code)
-    const accessToken = data.body['access_token']
-
-    api.setAccessToken(accessToken)
-
-    const apiLimit = 50
-    const results = await api.getMySavedTracks({ limit: apiLimit })
-    const total = 20 //results.body['total']
-    let songs = results.body.items.map((item) => item.track)
-
-    if (total > songs.length) {
-      for (let offset = apiLimit; offset < total; offset += apiLimit) {
-        const results = await api.getMySavedTracks({ limit: apiLimit, offset })
-        songs = [...songs, ...results.body.items.map((item) => item.track)]
-      }
-    }
-
-    return { props: { songs } }
-  } catch (err) {
-    return { notFound: true }
-  }
-}
