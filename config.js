9ba2a161526af87db0a8ab42534f9ec3ff2a6c3c
Pablo P Varela <pablo@pablopunk.com>
2021-07-13 14:34:51 +0200

    fix production domains

--- config.js
+++ config.js
@@ -2,8 +2,10 @@ module.exports = {
   SITE_NAME: 'EXPORTIFY',
   SITE_URL:
     process.env.NODE_ENV === 'production'
-      ? process.env.NEXT_PUBLIC_VERCEL_URL
-      : process.env.SITE_URL || 'http://localhost:3000',
+      ? process.env.VERCEL_ENV === 'production'
+        ? 'https://exportify.pablopunk.com' // real production
+        : process.env.NEXT_PUBLIC_VERCEL_URL // production preview on vercel
+      : process.env.SITE_URL || 'http://localhost:3000', // dev
   SITE_DESCRIPTION: 'Save all your spotify music as mp3 files from youtube',
   COPYRIGHT: 'Pablo Varela',
 }
