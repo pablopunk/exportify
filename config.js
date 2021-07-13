9cce221b789750eb2dc8941a26c626e64b406715
Pablo P Varela <pablo@pablopunk.com>
2021-07-13 12:58:58 +0200

    Use vercel's url

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ module.exports = {
   SITE_NAME: 'EXPORTIFY',
   SITE_URL:
     process.env.NODE_ENV === 'production'
-      ? 'https://exportify.pablopunk.com'
+      ? process.env.NEXT_PUBLIC_VERCEL_URL
       : process.env.SITE_URL || 'http://localhost:3000',
   SITE_DESCRIPTION: 'Save all your spotify music as mp3 files from youtube',
   COPYRIGHT: 'Pablo Varela',
